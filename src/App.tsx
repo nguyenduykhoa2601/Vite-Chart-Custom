986a85ec558190ba3a5bc7a8a01545b637b3bfe3
dev_khoa <nguyenduykhoa2601@gmail.com>
2022-06-07 14:43:03 +0700

    add pie

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Chart from "./Chart";
+import Loader from "./Loader/Loader";
 
 export default function App() {
-  const data = [
-    40, 20, 30, 40, 50, 60, 70, 80, 90, 100, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-    11, 11, 11, 11, 11, 11, 11,
-  ];
+  const data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
   const data1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
   const color = "#42bcf5";
   return (
     <div>
-      <Chart data={data} color={color} mode={'line'} />
+      <Chart data={data} color={color} mode={"pie"} />
+      {/* <Loader /> */}
     </div>
   );
 }
--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,13 +1,69 @@
 import { useEffect, useState } from "react";
 import useWindowSize from "./hooks/useWindowsize";
-import './App.css'
+import "./App.scss";
 
 export default function Chart(props: any) {
   const { mode, data, color } = props;
   const { width, height } = useWindowSize();
 
   const barWidth: number = width / (data.length ?? 1);
+  const randomColor = (i: number) => {
+    return Math.floor(Math.random() * i * 16777215).toString(16);
+  };
+
   let svgHeight = Math.max(...data) < height ? Math.max(...data) : height - 130;
+
+  const drawPieSlice = (
+    ctx: any,
+    centerX: any,
+    centerY: any,
+    radius: any,
+    startAngle: any,
+    endAngle: any,
+    color: any
+  ) => {
+    console.log("object", startAngle, "-----", endAngle);
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.closePath();
+    ctx.fill();
+  };
+  var canvas: HTMLCanvasElement = document.getElementById("myCanvas");
+  const countSum = (arr: Array<number>, time: number) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (i < time) {
+        sum += arr[i];
+      }
+    }
+    return sum;
+  };
+
+  useEffect(() => {
+    if (mode === "pie" && canvas && data.length > 0) {
+      var ctx = canvas.getContext("2d");
+
+      const sumData = data.reduce((total: any, num: any) => {
+        return total + num;
+      }, 0);
+      let prevStart = 0;
+      ctx?.beginPath();
+      for (let i = 0; i < data.length; i++) {
+        drawPieSlice(
+          ctx,
+          150,
+          150,
+          150,
+          prevStart,
+          (Math.PI * 2 * data[i]) / sumData + prevStart,
+          "#" + randomColor(i)
+        );
+        prevStart = (Math.PI * 2 * data[i]) / sumData + prevStart;
+      }
+    }
+  }, [canvas]);
 
   if (mode === "bars" || mode === undefined) {
     return (
@@ -40,7 +96,7 @@ export default function Chart(props: any) {
                   x1={barWidth * i}
                   y1={svgHeight - element}
                   x2={barWidth * (i + 1)}
-                  y2={svgHeight - data[i+1]}
+                  y2={svgHeight - data[i + 1]}
                   stroke-width="1"
                   stroke="black"
                 />
@@ -51,4 +107,13 @@ export default function Chart(props: any) {
       </div>
     );
   }
+
+  return (
+    <canvas
+      id="myCanvas"
+      width={300}
+      height={300}
+      style={{ transform: "rotate(-90deg)" }}
+    ></canvas>
+  );
 }
